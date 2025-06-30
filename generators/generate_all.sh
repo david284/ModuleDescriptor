9a4ec777534861da896504796ac9838808ff4f23
Sven Rosvall <sven@rosvall.ie>
2025-06-30 12:40:18 +0100

    Python generator for CANMIO. Use new generator in generate_all.sh

--- generators/generate_all.sh
+++ generators/generate_all.sh
@@ -30,24 +30,24 @@ $gen_dir/generate_CANMIO-SVO.sh -t CANSERVO8C | writeIfUpdated "$merg_dir"/CANSE
 
 # CANMIO
 # Default processor is PIC18F26K80
-$gen_dir/generate_CANMIO.sh -v 3a | writeIfUpdated "$merg_dir"/CANMIO-A520-3a.json
-$gen_dir/generate_CANMIO.sh -v 3c | writeIfUpdated "$merg_dir"/CANMIO-A520-3c.json
-$gen_dir/generate_CANMIO.sh -v 3d | writeIfUpdated "$merg_dir"/CANMIO-A520-3d.json
-$gen_dir/generate_CANMIO.sh -v 3e | writeIfUpdated "$merg_dir"/CANMIO-A520-3e.json
-$gen_dir/generate_CANMIO.sh -v 4a | writeIfUpdated "$merg_dir"/CANMIO-A520-4a.json
-$gen_dir/generate_CANMIO.sh -v 4b | writeIfUpdated "$merg_dir"/CANMIO-A520-4b.json
+python $gen_dir/generate_CANMIO.py -v 3a | writeIfUpdated "$merg_dir"/CANMIO-A520-3a.json
+python $gen_dir/generate_CANMIO.py -v 3c | writeIfUpdated "$merg_dir"/CANMIO-A520-3c.json
+python $gen_dir/generate_CANMIO.py -v 3d | writeIfUpdated "$merg_dir"/CANMIO-A520-3d.json
+python $gen_dir/generate_CANMIO.py -v 3e | writeIfUpdated "$merg_dir"/CANMIO-A520-3e.json
+python $gen_dir/generate_CANMIO.py -v 4a | writeIfUpdated "$merg_dir"/CANMIO-A520-4a.json
+python $gen_dir/generate_CANMIO.py -v 4b | writeIfUpdated "$merg_dir"/CANMIO-A520-4b.json
 
 # Processor P18F27Q83
-$gen_dir/generate_CANMIO.sh -p23 -v 4a | writeIfUpdated "$merg_dir"/CANMIO-A520-4a--P23.json
-$gen_dir/generate_CANMIO.sh -p23 -v 4b | writeIfUpdated "$merg_dir"/CANMIO-A520-4b--P23.json
+python $gen_dir/generate_CANMIO.py -p23 -v 4a | writeIfUpdated "$merg_dir"/CANMIO-A520-4a--P23.json
+python $gen_dir/generate_CANMIO.py -p23 -v 4b | writeIfUpdated "$merg_dir"/CANMIO-A520-4b--P23.json
 
 # Extended CANMIO
 # Default processor PIC18F46K80
-$gen_dir/generate_CANMIO.sh -t XIO -v 3e | writeIfUpdated "$merg_dir"/CANXIO-A540-3e.json
-$gen_dir/generate_CANMIO.sh -t XIO -v 4a | writeIfUpdated "$merg_dir"/CANXIO-A540-4a.json
-$gen_dir/generate_CANMIO.sh -t XIO -v 4b | writeIfUpdated "$merg_dir"/CANXIO-A540-4b.json
-$gen_dir/generate_CANMIO.sh -t XIO -p22 -v 4a | writeIfUpdated "$merg_dir"/CANXIO-A540-4a--P21.json
-$gen_dir/generate_CANMIO.sh -t XIO -p22 -v 4b | writeIfUpdated "$merg_dir"/CANXIO-A540-4b--P21.json
+python $gen_dir/generate_CANMIO.py -t XIO -v 3e | writeIfUpdated "$merg_dir"/CANXIO-A540-3e.json
+python $gen_dir/generate_CANMIO.py -t XIO -v 4a | writeIfUpdated "$merg_dir"/CANXIO-A540-4a.json
+python $gen_dir/generate_CANMIO.py -t XIO -v 4b | writeIfUpdated "$merg_dir"/CANXIO-A540-4b.json
+python $gen_dir/generate_CANMIO.py -t XIO -p22 -v 4a | writeIfUpdated "$merg_dir"/CANXIO-A540-4a--P21.json
+python $gen_dir/generate_CANMIO.py -t XIO -p22 -v 4b | writeIfUpdated "$merg_dir"/CANXIO-A540-4b--P21.json
 
 # CANPAN
 $gen_dir/generate_CANPAN.sh -v 1Y | writeIfUpdated "$merg_dir"/CANPAN-A51D-1Y.json
